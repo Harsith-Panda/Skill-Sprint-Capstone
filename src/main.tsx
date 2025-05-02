0e1db794d992e4e7fb8381f86d6ebc0afac82329
Harsith Priyan Sivanandham <harsithpriyan@gmail.com>
2025-05-02 17:23:58 +0530

    Added Pages. Completed router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import Login from './pages/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
